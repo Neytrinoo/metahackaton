702553a8f5276a4c79022fbc523e98b5471044cd
Neytrinoo <neytrino070902@yandex.ru>
2019-05-14 17:59:02 +0300

    asdf

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(100))
     password_hash = db.Column(db.String(400))
     token = db.Column(db.String(30))
+    telegram_id = db.Column(db.String(100))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
