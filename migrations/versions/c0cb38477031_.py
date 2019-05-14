90419c4c77297a5410a7de9d8416609e9e56291f
HehexOne <werty.mn2@yandex.ru>
2019-05-14 16:26:07 +0300

    REST fix

--- migrations/versions/c0cb38477031_.py
+++ migrations/versions/c0cb38477031_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 69582594820a
+Revision ID: c0cb38477031
 Revises: 
-Create Date: 2019-05-14 11:06:12.428382
+Create Date: 2019-05-14 16:23:49.517708
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '69582594820a'
+revision = 'c0cb38477031'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,6 +23,7 @@ def upgrade():
     sa.Column('username', sa.String(length=100), nullable=True),
     sa.Column('name', sa.String(length=100), nullable=True),
     sa.Column('password_hash', sa.String(length=400), nullable=True),
+    sa.Column('token', sa.String(length=30), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
